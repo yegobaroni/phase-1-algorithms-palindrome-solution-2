a76afa24c8f9a1b43125eb087a44c3720b4fa81a
yego <yegokipkemoi254@gmail.com>
2022-08-29 16:07:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
+  return true;
+}
 /* 
   Add your pseudocode here
 */
@@ -23,3 +31,21 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+describe("isPalindrome", () => {
+  it("returns true for 'abba'", () => {
+    expect(isPalindrome("abba")).to.be.true;
+  });
+  it("returns true for 'racecar'", () => {
+    expect(isPalindrome("racecar")).to.be.true;
+  });
+  it("returns true for 'a'", () => {
+    expect(isPalindrome("a")).to.be.true;
+  });
+  it("returns false for 'robot'", () => {
+    expect(isPalindrome("robot")).to.be.false;
+  });
+  it("returns false for 'ab'", () => {
+    expect(isPalindrome("ab")).to.be.false;
+  });
+});
